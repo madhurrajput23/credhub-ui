c33252c376f3eb5a24c833f96efd4f1f9956d94c
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2023-01-12 03:33:51 +0530

    added loan eligibilty component

--- src/components/loan-data/LoanData.js
+++ src/components/loan-data/LoanData.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import "./LoanData.css";
+import { loanEligibilityList } from './LoanEligibiltyList.data';
+
 
 const LoanData = () => {
   return (
-    <div>LoanData</div>
+    <div className='loandata_main'>
+      <h2>Eligibility</h2>
+      <div className='loandata_box'>
+      <ul className='loandata_list'>{
+        loanEligibilityList.map((list) =><li className='primary_color' key={list}>{list}</li>)
+      }
+
+        
+      </ul>
+      <div className='loandata_img'>
+        <img src='./images/eligibility_img.svg' alt='eligibility_img'/>
+      </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/loan-data/LoanEligibiltyList.data.js
@@ -0,0 +1,8 @@
+export const loanEligibilityList = [
+    "Minimum Age: 18 years and Maximum Age: 70 years",
+    "The applicant must be Indian Resident",
+    "The applicant can be (any one): Salaried/ Self-employed",
+     "At least Rs.5-6 lakh depending on the type of employment",
+     "The applicant must have (any one): A permanent residence/ A rented residence where he/she has resided for at least a year prior to applying for a loan",
+     "A good credit score of at least 750 or more obtained from a recognised credit bureau"
+]
