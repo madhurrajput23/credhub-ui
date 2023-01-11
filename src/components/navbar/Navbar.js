16a542fa9c21b9ab7c95b9ab1d0919674bec0e1e
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2023-01-11 23:39:44 +0530

    added navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,23 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar_main'>
+       <div className='navbar_logo'> 
+       <img src='./images/Navbar-Logo.svg' alt='logo' />
+       </div> 
+       <div className='navbar_menu'>
+        <div>HOME</div>
+        <div>LOAN</div>
+        <div>PRODUCT</div>
+        <div>PARTNER WITH US</div>
+        <div>LOCATE US</div>
+       </div>
+       <div className='navbar_buttons'>
+        <button className='primary_outline_button'>Login</button>
+        <button className='primary_button'>Request Demo</button>
+       </div>
+    
+    </div>
   )
 }
 
