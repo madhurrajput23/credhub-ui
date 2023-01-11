2aa909d6b1189ca02a03f118430475976770f12c
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2023-01-12 00:46:13 +0530

    calculator layout

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
   return (
-    <div>Calculator</div>
-  )
-}
+    <div className="calculator_main">
+      <h3 className="primary_color">Home loan with attractive interest rate</h3>
+      <div className="calculator_box">
+        <div className="calculator_range">
+          range
+        </div>
+        <div className="calculator_graph">
+          graph
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Calculator
+export default Calculator;
