c0a939527162ba98a351316d2b32e75d51fcded3
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2023-01-12 00:06:43 +0530

    added banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="banner_main">
+      <div className="banner_text">
+        There is nothing like <span>Staying at your own house</span> with real comfort.
+      </div>
+      <div className="banner_img">
+      <img src="/images/home_loan_banner.svg" alt="banner"/>
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
