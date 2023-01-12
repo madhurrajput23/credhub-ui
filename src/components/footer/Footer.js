6e367789c04c62b5f4a697993f960c9864419332
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2023-01-12 22:19:28 +0530

    added footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,44 +1,58 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer_main'>
-      <div className='footer_logo_box'>
-        <div className='footer_logo'>
-          <img src='./images/footer_logo.svg' alt='logo'/>
+    <div className="footer_main">
+      <div className="footer_logo_box">
+        <div className="footer_logo">
+          <img src="./images/footer_logo.svg" alt="logo" />
         </div>
-        <div className='footer_logo_title'>Credhub</div>
+        <div className="footer_logo_title">Credhub</div>
       </div>
-      <div className='footer_content'>
-        <div className='footer_content_left'>
-          <div className='footer_content_left_list'>
-            <div>Company</div>
+      <div className="footer_content">
+        <div className="footer_content_left">
+          <div className="footer_content_left_list">
+            <div className="footer_content_left_list_heading">Company</div>
             <div>About us</div>
             <div>News and Media</div>
             <div>Contact us</div>
           </div>
 
-          <div className='footer_content_left_list'>
-            <div>Link</div>
+          <div className="footer_content_left_list">
+            <div className="footer_content_left_list_heading">Link</div>
             <div>Terms of use</div>
             <div>Privacy Policy</div>
             <div>Site Map</div>
           </div>
 
-          <div className='footer_content_left_list'>
-            <div>Our Solution</div>
+          <div className="footer_content_left_list">
+            <div className="footer_content_left_list_heading">Our Solution</div>
             <div>Commercial</div>
             <div>Non-Commercial</div>
             <div>Featured Solution</div>
           </div>
         </div>
-        <div className='footer_content_right'></div>
-        
+        <div className="footer_content_right">
+          <div className="send_message">
+            <img src="./images/message-logo.svg" alt="message_logo"/>
+            <div className="send_message_text">Send a request</div>
+          </div>
+          <div className="footer_links">
+            <div className="footer_links_text">Follow Us</div>
+            <div className="footer_links_img">
+              <img src="./images/linked-in-img.svg" alt="linked-in-logo"/>
+              <img src="./images/facebook-img.svg" alt="facebook-logo"/>
+              <img src="./images/twitter-img.svg" alt="twitter-logo"/>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='footer_copyright'>Copyright © Credhub All Rights Reserved.</div>
+      <div className="footer_copyright">
+        Copyright © Credhub All Rights Reserved.
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
