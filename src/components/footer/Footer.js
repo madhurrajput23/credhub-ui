3d77b9d8cf813b92e69d204396020a5d98b4613f
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2023-01-12 18:53:40 +0530

    added apply component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,41 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer_main'>
+      <div className='footer_logo_box'>
+        <div className='footer_logo'>
+          <img src='./images/footer_logo.svg' alt='logo'/>
+        </div>
+        <div className='footer_logo_title'>Credhub</div>
+      </div>
+      <div className='footer_content'>
+        <div className='footer_content_left'>
+          <div className='footer_content_left_list'>
+            <div>Company</div>
+            <div>About us</div>
+            <div>News and Media</div>
+            <div>Contact us</div>
+          </div>
+
+          <div className='footer_content_left_list'>
+            <div>Link</div>
+            <div>Terms of use</div>
+            <div>Privacy Policy</div>
+            <div>Site Map</div>
+          </div>
+
+          <div className='footer_content_left_list'>
+            <div>Our Solution</div>
+            <div>Commercial</div>
+            <div>Non-Commercial</div>
+            <div>Featured Solution</div>
+          </div>
+        </div>
+        <div className='footer_content_right'></div>
+        
+      </div>
+      <div className='footer_copyright'>Copyright © Credhub All Rights Reserved.</div>
+    </div>
   )
 }
 
--- src/components/verification-process/VerificationProcess.js
+++ src/components/verification-process/VerificationProcess.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React from "react";
 import "./VerificationProcess.css";
 
 const VerificationProcess = () => {
   return (
-    <div>VerificationProcess</div>
-  )
-}
+    <div className="verificationprocess_main">
+      <div className="verificationprocess_heading">
+        Verification <span>Process</span>
+      </div>
+      <div className="verificationprocess_contentbox">
+        <div className="verificationprocess_text">
+          <div className="verificationprocess_text_item process_item_active">
+            <span className="verificationprocess_listitem_bullet"></span>Loan
+            representative will connnect you
+          </div>
+          <div className="verificationprocess_text_item">
+            <span className="verificationprocess_listitem_bullet"></span>
+            Document collection will be done
+          </div>
+          <div className="verificationprocess_text_item">
+            <span className="verificationprocess_listitem_bullet"></span>
+            Document verification will be done
+          </div>
+          <div className="verificationprocess_text_item">
+            <span className="verificationprocess_listitem_bullet"></span>
+            Finally, eSign the Loan agreement
+          </div>
+        </div>
+        <div className="verificationprocess_img">
+          <img src="./images/verification_img.svg" alt="verification-img" />
+        </div>
+      </div>
+      <div className="verificationprocess_applybox">
+        <div className="verificationprocess_interest_info">
+          <div className="verificationprocess_interest_info_title">
+            Rate of Interest
+          </div>
+          <div className="verificationprocess_interest_info_text">
+            The rate of interest for home loans differs from one lender to
+            another and also it depends on some factors like Credit Score,
+            tenure of the loan, quantum of the loan and your income. We have
+            interest rate starting as low as 7.70%.
+            <br /> *The rate is subject to change as per the lender.
+          </div>
+        </div>
+        <div className="verificationprocess_apply_button_box">
+          <div className="verificationprocess_apply_info">
+            Get your dream home with Credhub
+          </div>
+          <div className="verificationprocess_apply_infomore">
+            Step up for best <b>Home Loan</b> offerings tailored specially for you
+          </div>
+          <button className="primary_button">Apply now</button>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default VerificationProcess
+export default VerificationProcess;
